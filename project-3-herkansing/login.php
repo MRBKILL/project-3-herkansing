92a93fda17e5d2f3338fb3e3bc772066b82a36c6
MRBNIGG <9019917@student.zadkine.nl>
2023-05-08 10:53:11 +0200

    crud toegevoegd

--- project-3-herkansing/login.php
+++ project-3-herkansing/login.php
@@ -114,7 +114,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     }        
     ?>
 
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+    <form action="" method="post">
         <div class="form-group">
             <label>Gebruikersnaam</label>
             <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
